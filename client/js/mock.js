650902479f94f222f54b637b796ab54e1043ab5c
Juanfran <juanfran.alcantara@kaleidos.net>
2013-07-16 08:55:14 +0200

    init mocks api

---
+++ client/js/mock.js
@@ -0,0 +1,21 @@
+app.api = {};
+
+app.api.getTeam1Dwafs = function(){
+    return [
+        {x: 20, y: 20},
+        {x: 19, y: 20},
+        {x: 18, y: 20},
+        {x: 17, y: 20},
+        {x: 16, y: 20}
+    ];
+};
+
+app.api.getTeam2Dwafs = function(){
+    return [
+        {x: 20, y: 0},
+        {x: 19, y: 0},
+        {x: 18, y: 0},
+        {x: 17, y: 0},
+        {x: 16, y: 0}
+    ];
+};
